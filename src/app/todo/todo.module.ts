8058063d8bde3e056f24fa95e3f04f1583a0900a
Shreedhar Hegde <shreedharhegde@Shreedhars-MacBook-Air.local>
2022-09-23 16:49:48 +0530

    added plus button

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListTodoComponent } from './list-todo/list-todo.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [ListTodoComponent],
-  imports: [CommonModule, DragDropModule],
+  imports: [CommonModule, DragDropModule, MatIconModule, MatButtonModule],
   exports: [ListTodoComponent],
 })
 export class TodoModule {}
