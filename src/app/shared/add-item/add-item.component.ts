ffcd2fbcd4170d2c5ed21b7e5b7a23cd7ccf1428
Shreedhar Hegde <shreedharhegde@Shreedhars-MacBook-Air.local>
2022-09-24 12:11:37 +0530

    feat:input to add date on todo

--- src/app/shared/add-item/add-item.component.ts
+++ src/app/shared/add-item/add-item.component.ts
@@ -8,10 +8,16 @@ import { Todo } from 'src/app/todo/todo.service';
   styleUrls: ['./add-item.component.scss'],
 })
 export class AddItemComponent {
+  minDate!: Date;
+  maxDate!: Date;
   constructor(
     public dialogRef: MatDialogRef<AddItemComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Todo
-  ) {}
+  ) {
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date();
+    this.maxDate = new Date(currentYear + 1, 11, 31);
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
