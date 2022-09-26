e1c3632d7bb87fbf5e285d15fa1ab89ccc8ccac9
Shreedhar Hegde <shreedharhegde@Shreedhars-MacBook-Air.local>
2022-09-26 19:12:48 +0530

    feat: authguard

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -2,6 +2,7 @@ import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
+  CanActivate,
   Router,
   RouterStateSnapshot,
 } from '@angular/router';
@@ -10,23 +11,24 @@ import { map, Observable, tap } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuardService {
-  constructor(
-    private router: Router,
-    private socialAuthService: SocialAuthService
-  ) {}
-
+export class AuthGuardService implements CanActivate {
+  constructor(private authService: SocialAuthService, private router: Router) {}
+  user!: SocialUser;
+  loggedIn: boolean = false;
   canActivate(
-    route: ActivatedRouteSnapshot,
+    next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): Observable<boolean> {
-    return this.socialAuthService.authState.pipe(
-      map((socialUser: SocialUser) => !!socialUser),
-      tap((isLoggedIn: boolean) => {
-        if (!isLoggedIn) {
-          this.router.navigate(['']);
-        }
-      })
-    );
+  ): boolean {
+    if (localStorage.getItem('user')) {
+      this.user = JSON.parse(localStorage.getItem('user')!);
+      this.loggedIn = true;
+    } else {
+      this.authService.authState.subscribe((user) => {
+        this.user = user;
+        this.loggedIn = user != null;
+      });
+      this.router.navigateByUrl('');
+    }
+    return this.loggedIn ? true : false;
   }
 }
