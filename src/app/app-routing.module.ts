6eef42f3506dbd315cbd821252344a1088a9ce36
Shreedhar Hegde <shreedharhegde@Shreedhars-MacBook-Air.local>
2022-09-26 19:11:14 +0530

    feat: added authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './auth/auth-guard.service';
 import { SigninComponent } from './auth/signin/signin.component';
 import { ListTodoComponent } from './todo/list-todo/list-todo.component';
 
@@ -7,6 +8,7 @@ const routes: Routes = [
   { path: '', component: SigninComponent },
   {
     path: 'task-tracker',
+    canActivate: [AuthGuardService],
     component: ListTodoComponent,
   },
 ];
