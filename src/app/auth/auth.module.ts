318e40bdb6af60a29e22c60d6a9566f3d66303a7
Shreedhar Hegde <shreedharhegde@Shreedhars-MacBook-Air.local>
2022-09-26 19:13:24 +0530

    update: import social login module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SigninComponent } from './signin/signin.component';
-
-
+import { SocialLoginModule } from '@abacritt/angularx-social-login';
 
 @NgModule({
-  declarations: [
-    SigninComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [SigninComponent],
+  imports: [CommonModule, SocialLoginModule],
 })
-export class AuthModule { }
+export class AuthModule {}
